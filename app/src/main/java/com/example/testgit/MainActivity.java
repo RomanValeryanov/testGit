8ca13c51fd5d74440990d85c32dc3b76ca00725b
romichBaza <romich271@mail.ru>
2024-04-03 01:03:35 +0300

    Remove

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         i = 0;
-gg=0;
+
     }
 }
